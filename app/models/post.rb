1be7e76e3b9352876ff4abab9254a2f4f9d31f6a
nachosala89 <nachosala89@gmail.com>
2022-01-17 19:51:57 -0300

    Fix linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comment
   has_many :like
-  
+
   def update_counter(user_id = author_id)
     user = User.find(user_id)
     user.posts_counter = Post.where(author_id: user.id).count
